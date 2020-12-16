360f193a66acd6abd818cf8b62bec4c9b141a38a
KBD2 <44832995+KBD2@users.noreply.github.com>
2020-12-16 20:25:35 +1300

    Day 15

---
+++ 15/solution2.py
@@ -0,0 +1,21 @@
+start = list(map(lambda x: int(x), input("Input? >").split(',')))
+
+count = 1
+spoken = {}
+lastNum = start[-1]
+
+for item in start[:-1]:
+    spoken[item] = count
+    count += 1
+
+while count < 30000000:
+    if lastNum not in spoken:
+        spoken[lastNum] = count
+        lastNum = 0
+    else:
+        nextNum = count - spoken[lastNum]
+        spoken[lastNum] = count
+        lastNum = nextNum
+    count += 1
+
+print(lastNum)
